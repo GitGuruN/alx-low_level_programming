0140d729c5c44adfd769571fdc716ab6f7320daf
GitGuruN <bakmshalsbata@gmail.com>
2023-06-16 14:12:59 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@ int main(void)
 {
 char ch = 'a';
 
-while (ch <= 'z') 
-{       
-if (ch != 'e' && ch != 'q') 
-{      
+while (ch <= '')
+{
+if (ch != 'e' && ch != 'q')
+{
 putchar(ch);
 }
-ch++;   
+ch++;
 }
 putchar('\n');
 return (0);
