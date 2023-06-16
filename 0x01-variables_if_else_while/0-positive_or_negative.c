fbb8a31e20716392b02bcebbe1305c738b158731
GitGuruN <bakmshalsbata@gmail.com>
2023-06-16 06:21:23 -0700

    0. Positive anything is better than negative nothing git add .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,16 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if(n=0){
+if(n = 0)
+{
 printf("is zero");
-}else if(n>0){
+}
+else if(n > 0)
+{
 printf("is positive");
-}else{
+}
+else
+{
 printf("is negative");
 }
 return (0);
