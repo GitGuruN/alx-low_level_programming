35a5b453c1e0153f033a7c5d4b19336e0c448807
GitGuruN <bakmshalsbata@gmail.com>
2023-06-26 18:15:44 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to be converted.
- *
- * Description: This function converts the string pointed to by s to an integer.
- * It takes into account any preceding '+' or '-' signs before the number.
- * If there are no numbers in the string, the function returns 0.
- *
- * Return: The converted integer value.
- */
-int _atoi(char *s)
-{
-int sign = 1;
-int result = 0;
-int i = 0;
-unsigned int ni = 0;
+#include <string.h>
 
-while (s[i])
+/**
+ * _strcpy - Copies a string from src to dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Description: This function copies the string pointed to by src, including the
+ * terminating null byte ('\0'), to the buffer pointed to by dest.
+ *
+ * Return: Pointer to the destination buffer (dest).
+ */
+char *_strcpy(char *dest, char *src)
 {
-if (s[i] == 45)
-{
-sign *= -1;
-}
-while (s[i] >= 48 && s[i] <= 57)
-{
-resultat = 1;
-ni = (ni * 10) + (s[i] - '0');
-i++;
-}
-if (resultat == 1)
-{
-break;
-}
-i++;
-}
-ni *= sign;
-return (ni);
+return (strcpy(dest, src));
 }
