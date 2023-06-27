4f1790334bc2193893ad2cff386216a8b6862c05
GitGuruN <bakmshalsbata@gmail.com>
2023-06-26 17:48:37 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string to be converted.
+ *
+ * Description: This function converts the string pointed to by s to an integer
+ * using the `atoi` function from the standard library.
+ *
+ * Return: The converted integer value.
+ */
+int _atoi(char *s)
+{
+return (atoi(s));
+}
