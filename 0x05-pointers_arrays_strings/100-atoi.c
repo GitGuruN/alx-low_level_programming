179b3056a2d8e6125a204114ba3b746b51054e59
GitGuruN <bakmshalsbata@gmail.com>
2023-06-26 19:29:07 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,39 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to be converted.
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Description: This function converts the string pointed to by s to an integer
- * using the `atoi` function from the standard library.
- *
- * Return: The converted integer value.
+ * Return: The converted integer
  */
 int _atoi(char *s)
 {
-return (atoi(s));
+int i = 0;
+int neg_count = 0;
+int result = 0;
+int len = 0;
+int found_digit = 0;
+int digit_value = 0;
+
+while (s[len] != '\0')
+len++;
+while (i < len && found_digit == 0)
+{
+if (s[i] == '-')
+neg_count++;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit_value = s[i] - '0';
+if (neg_count % 2)
+digit_value = -digit_value;
+result = result * 10 + digit_value;
+found_digit = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+found_digit = 0;
+}
+i++;
+}
+if (found_digit == 0)
+return (0);
+return (result);
 }
