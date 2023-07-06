e2a39893fcae9a216a6c6dc652ef91c9ecd56852
GitGuruN <bakmshalsbata@gmail.com>
2023-07-05 17:06:29 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * sqrt_recursive - Helper function to calculate the natural square root recursively.
- * @n: The number to calculate the square root of.
- * @guess: The current guess for the square root.
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number to be used
+*
+* Return: the square root of n
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
+}
+
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
  *
- * Return: The square root if the guess is correct, -1 if the guess exceeds the square root.
+ * Return: the square root of n
  */
-int _sqrt_recursive(int n, int guess)
+
+int _sqrt(int n, int x)
 {
-if (guess * guess == n)
-{
-return (guess);
-}
-else if (guess * guess > n)
-{
-return (-1);
-}
-else
-{
-return (_sqrt_recursive(n, guess + 1));
-}
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
